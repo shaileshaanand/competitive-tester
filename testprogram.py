b65a424e90dc374a0727d5089b39dc3ca2d7427a
Shailesh Aanand <anaandshailu@gmail.com>
2020-06-14 18:59:48 +0530

    make it windows compatible

--- testprogram.py
+++ testprogram.py
@@ -3,11 +3,12 @@ import argparse
 import json
 import subprocess
 import os
+import sys
 
 
 parser = argparse.ArgumentParser(
     description="A tester to test your comepitive coding solution against test cases")
-language = parser.add_mutually_exclusive_group()
+language = parser.add_mutually_exclusive_group(required=True)
 
 parser.add_argument('program_file',
                     help="path to the program", type=str)
@@ -17,7 +18,6 @@ language.add_argument('--exe', action='store_true',
                       help="call program by './filename'")
 parser.add_argument('-t', '--test-file',
                     help="path to the tests json file", required=True, type=open)
-parser.set_defaults(python=True)
 args = parser.parse_args()
 test_cases = json.loads(args.test_file.read())
 total_cases = len(test_cases)
@@ -25,16 +25,27 @@ failed_cases = 0
 for i, test_case in enumerate(test_cases, 1):
     input_data = "\n".join(test_case["input"])
     required_output = "\n".join(test_case["output"])
-    if args.python:
-        process = subprocess.run(["python", args.program_file],
-                                 input=input_data.encode(),
-                                 capture_output=True)
-
-    elif args.exe:
-        process = subprocess.run([f".{os.path.sep}args.program_file"],
-                                 input=input_data.encode(),
-                                 capture_output=True)
-    actual_output = process.stdout.decode().strip().replace('\r','')
+    if sys.platform == 'linux':
+        if args.exe:
+            process = subprocess.run([f".{os.path.sep}args.program_file"],
+                                     input=input_data.encode(),
+                                     capture_output=True)
+        elif args.python:
+            process = subprocess.run(["python3", args.program_file],
+                                     input=input_data.encode(),
+                                     capture_output=True)
+    elif sys.platform == 'win32':
+        if args.exe:
+            process = subprocess.run([f"{args.program_file}"],
+                                     input=input_data.encode(),
+                                     capture_output=True,
+                                     shell=True)
+        elif args.python:
+            process = subprocess.run(["python", args.program_file],
+                                     input=input_data.encode(),
+                                     capture_output=True,
+                                     shell=True)
+    actual_output = process.stdout.decode().strip().replace('\r', '')
     if process.returncode == 0 and required_output == actual_output:
         print(f"Test Case {i} Passed  ✅")
     else:
